9e73d9540035fb27eb594314703f8e0269906408
DBHANUPRASAD <136061102+DBHANUPRASAD@users.noreply.github.com>
2023-09-13 19:16:18 +0530

    Create file.java
    javafile

---
+++ file.java
@@ -0,0 +1,12 @@
+public class file
+  {
+    public static void main(String args[])
+    {
+      int a,b,c;
+      a=10;
+      b=20;
+      c=a+b;
+      int avg=c/2;
+      System.out.println("the sum and average of two numbers is:"+avg);
+    }
+  }
